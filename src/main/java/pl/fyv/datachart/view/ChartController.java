f04116c47496c61b3dbcaa132b8ca2e43ffe54fb
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2018-05-26 20:04:21 +0200

    TODO: update methods, creating FileChooser dialog window

--- src/main/java/pl/fyv/datachart/view/ChartController.java
+++ src/main/java/pl/fyv/datachart/view/ChartController.java
@@ -1,5 +1,44 @@
 package pl.fyv.datachart.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import pl.fyv.datachart.Main;
+
+import java.awt.event.ActionEvent;
+
+
 public class ChartController {
-//    TODO
+
+    private Main main;
+    private Stage stage;
+
+    @FXML
+    private Button loadFile;
+    private FileChooser fileChooser = new FileChooser();
+
+    public ChartController() {}
+
+    @FXML
+    private void initialize() {
+        loadFile.setOnAction(this::callFileChooser);
+    }
+
+    @FXML
+    private void callFileChooser(ActionEvent actionEvent) {
+
+    }
+
+
+
+
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
 }
